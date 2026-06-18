cfca0466845547c5336291730faf54d27a7ca834
GADGETN1K <playgadgetnik@gmail.com>
2026-06-18 16:35:21 +0000

    fire: Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+# Kernel
+git clone https://github.com/MoonL1ght-Team/MoonLightKernel_xiaomi_heat_fire -b main kernel/xiaomi/fire
+
+# Vendor tree
+git clone https://github.com/MoonL1ght-Team/proprietary_vendor_xiaomi_fire -b lineage-23.2 vendor/xiaomi/fire
+
+# Xiaomi x MTK etc.
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
