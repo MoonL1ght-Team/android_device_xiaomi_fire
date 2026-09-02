82a6359e44c2ba9bb0ef8a230ef778369d2b718d
1VicTim1 <122aaa121@gmail.com>
2026-09-02 08:05:34 +0500

    fire: sync first-stage contract with HOS2

--- lineage_fire.mk
+++ lineage_fire.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := fire
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fire-user 14 UP1A.231005.007 V816.0.8.0.UMXMIXM release-keys" \
-    BuildFingerprint=Redmi/fire_global/fire:14/UP1A.231005.007/V816.0.8.0.UMXMIXM:user/release-keys \
+    BuildDesc="vnd_fire-user 15 AP3A.240905.015.A2 OS2.0.209.0.VMXMIXM release-keys" \
+    BuildFingerprint=Redmi/vnd_fire/fire:15/AP3A.240905.015.A2/OS2.0.209.0.VMXMIXM:user/release-keys \
     SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
     SystemName=$(PRODUCT_SYSTEM_NAME) \
     ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
